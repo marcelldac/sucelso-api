9a8b8bede2e05096535307e1f34118bc03c84b3b
marcelldac <dacmarcell@gmail.com>
2023-10-02 02:50:01 -0300

    chore(serer): lint

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const PORT = 3000;
 
-const userRouter = require('./router/user');
-const healthRouter = require('./router/health');
-const loginRouter = require('./router/login');
-const subjectRouter = require('./router/subject');
-const teacherRouter = require('./router/teacher');
-const pubsRouter = require('./router/pubs');
+const userRouter = require("./router/user");
+const healthRouter = require("./router/health");
+const loginRouter = require("./router/login");
+const teacherRouter = require("./router/teacher");
+const gradeRouter = require("./router/grade");
+const courseRouter = require("./router/course");
+const coordinatorRouter = require("./router/coordinator");
 
 app.use(cors());
 app.use(express.json());
-app.use('/users', userRouter);
-app.use('/login', loginRouter);
-app.use('/health', healthRouter);
-app.use('/subjects', subjectRouter);
-app.use('/teachers', teacherRouter);
-app.use('/pubs', pubsRouter)
+app.use("/users", userRouter);
+app.use("/login", loginRouter);
+app.use("/health", healthRouter);
+app.use("/teachers", teacherRouter);
+app.use("/grades", gradeRouter);
+app.use("/courses", courseRouter);
+app.use("/coordinators", coordinatorRouter);
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
 
