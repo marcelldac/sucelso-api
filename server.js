89e3f404cf19f6003a2a60b143b8763ac796b723
marcelldac <dacmarcell@gmail.com>
2023-10-02 02:55:22 -0300

    feat: removes health endpoint and router

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = 3000;
 
 const userRouter = require("./router/user");
-const healthRouter = require("./router/health");
 const loginRouter = require("./router/login");
 const teacherRouter = require("./router/teacher");
 const gradeRouter = require("./router/grade");
@@ -16,7 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/login", loginRouter);
-app.use("/health", healthRouter);
 app.use("/teachers", teacherRouter);
 app.use("/grades", gradeRouter);
 app.use("/courses", courseRouter);
