96e18133537478fc56ba9731918bb8b1fb8ce90d
marcelldac <dacmarcell@gmail.com>
2023-10-02 02:08:33 -0300

    feat: add courses

---
+++ router/course.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const courseController = require("../controller/courseController");
+
+router.get("/", courseController.read);
+router.get("/:id", courseController.readById);
+router.post("/", courseController.create);
+router.put("/:id", courseController.update);
+router.delete("/:id", courseController.delete);
+
+module.exports = router;
