4091661b2cb51c5861201e8b5409e8b4354cefbc
marcelldac <dacmarcell@gmail.com>
2023-12-07 11:18:57 -0300

    refactor: better returns and ifs

--- controller/coordinatorController.js
+++ controller/coordinatorController.js
@@ -11,8 +11,8 @@ exports.create = async (req, res) => {
       email,
     },
   });
-  if (emailInUse != null) {
-    return res.status(403).send();
+  if (emailInUse) {
+    return res.status(403).json({ message: "O e-mail já está em uso" });
   }
   try {
     const coordinator = await prisma.coordinator.create({
@@ -23,49 +23,47 @@ exports.create = async (req, res) => {
         hierarchy: 2,
       },
     });
-    return res.status(201).json({ coordinator });
+    return res.status(201).json(coordinator);
   } catch (err) {
-    return res.status(500).json({ err });
+    return res.status(500).json({ error: err });
   }
 };
 //#endregion
 //#region Read Coordinator
 exports.read = async (req, res) => {
   const coordinator = await prisma.coordinator.findMany();
-  res.status(200).json({ coordinator });
+  res.status(200).json(coordinator);
 };
 exports.readById = async (req, res) => {
   const { id } = req.params;
-  const newId = parseInt(id);
-  if (!newId) {
-    return res.status(400).json({ msg: "Id Obrigatório" });
+  if (!id) {
+    return res.status(400).json({ message: "Id Obrigatório" });
   }
   const coordinator = await prisma.coordinator.findUnique({
-    where: { id: newId },
+    where: { id: parseInt(id) },
   });
   if (!coordinator) {
-    return res.status(404).json({ msg: "Coordenador Não Encontrado" });
+    return res.status(404).json({ message: "Coordenador Não Encontrado" });
   }
-  return res.status(200).json({ coordinator });
+  return res.status(200).json(coordinator);
 };
 //#endregion
 //#region Update Coordinator
 exports.update = async (req, res) => {
   const { id } = req.params;
-  const { name, email } = req.body;
-  const newId = parseInt(id);
-  if (!newId) {
-    res.status(400).json("Id obrigatório");
+  if (!id) {
+    res.status(400).json({ message: "Id obrigatório" });
   }
-  const coordinatorExists = await prisma.coordinator.findUnique({
-    where: { id: newId },
+  const { name, email } = req.body;
+  const doesCoordinatorExists = await prisma.coordinator.findUnique({
+    where: { id: parseInt(id) },
   });
-  if (!coordinatorExists) {
-    res.status(404).json("Professor não existente");
+  if (!doesCoordinatorExists) {
+    res.status(404).json({ message: "Coordenador não existente" });
   }
   const coordinator = await prisma.coordinator.update({
     where: {
-      id: newId,
+      id: parseInt(id),
     },
     data: {
       name,
@@ -78,17 +76,16 @@ exports.update = async (req, res) => {
 //#region Delete Coordinator
 exports.delete = async (req, res) => {
   const { id } = req.params;
-  const newId = parseInt(id);
-  if (!newId) {
-    return res.status(400).json({ msg: "Id Não Encontrado" });
+  if (!id) {
+    return res.status(400).json({ message: "Id Não Encontrado" });
   }
-  const coordinatorChecker = await prisma.coordinator.findUnique({
-    where: { id: newId },
+  const doesCoordinatorFound = await prisma.coordinator.findUnique({
+    where: { id: parseInt(id) },
   });
-  if (!coordinatorChecker) {
-    return res.status(404).json({ msg: "Professor Não Encontrado" });
+  if (!doesCoordinatorFound) {
+    return res.status(404).json({ message: "Professor Não Encontrado" });
   }
-  await prisma.coordinator.delete({ where: { id: newId } });
-  return res.status(204).send("Coordenador Removido");
+  await prisma.coordinator.delete({ where: { id: parseInt(id) } });
+  return res.status(204).json({ message: "Coordenador Removido" });
 };
 //#endregion
