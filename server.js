8d46d04553fee781eb7c6fb09a44374ce42c44f1
marcelldac <dacmarcell@gmail.com>
2023-12-09 01:54:45 -0300

    refactor: organize code

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const PORT = process.env.PORT || 3333;
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const userRouter = require("./router/user");
 const loginRouter = require("./router/login");
 const teacherRouter = require("./router/teacher");
 const gradeRouter = require("./router/grade");
 const courseRouter = require("./router/course");
 const coordinatorRouter = require("./router/coordinator");
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
 app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
@@ -16,6 +20,8 @@ app.use("/teachers", teacherRouter);
 app.use("/grades", gradeRouter);
 app.use("/courses", courseRouter);
 app.use("/coordinators", coordinatorRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
